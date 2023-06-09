90733347ae51243c202e2010c45e818eac22ec2f
Михайло Кондратенко <mike.kondr@gmail.com>
2023-06-09 19:23:03 +0300

    task3

---
+++ task3/frmCosts.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace task3
+{
+    partial class frmCosts
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvCosts = new System.Windows.Forms.DataGridView();
+            this.btnCreateMas = new System.Windows.Forms.Button();
+            this.numCostsCount = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCosts)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCostsCount)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvCosts);
+            this.groupBox1.Controls.Add(this.btnCreateMas);
+            this.groupBox1.Controls.Add(this.numCostsCount);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 97);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Вихідні дані";
+            // 
+            // dgvCosts
+            // 
+            this.dgvCosts.AllowUserToAddRows = false;
+            this.dgvCosts.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvCosts.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvCosts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCosts.Location = new System.Drawing.Point(214, 10);
+            this.dgvCosts.Name = "dgvCosts";
+            this.dgvCosts.Size = new System.Drawing.Size(556, 81);
+            this.dgvCosts.TabIndex = 3;
+            // 
+            // btnCreateMas
+            // 
+            this.btnCreateMas.Location = new System.Drawing.Point(22, 54);
+            this.btnCreateMas.Name = "btnCreateMas";
+            this.btnCreateMas.Size = new System.Drawing.Size(186, 30);
+            this.btnCreateMas.TabIndex = 2;
+            this.btnCreateMas.Text = "Згенерувати масив";
+            this.btnCreateMas.UseVisualStyleBackColor = true;
+            this.btnCreateMas.Click += new System.EventHandler(this.btnCreateMas_Click);
+            // 
+            // numCostsCount
+            // 
+            this.numCostsCount.Location = new System.Drawing.Point(155, 25);
+            this.numCostsCount.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numCostsCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numCostsCount.Name = "numCostsCount";
+            this.numCostsCount.Size = new System.Drawing.Size(53, 20);
+            this.numCostsCount.TabIndex = 1;
+            this.numCostsCount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Кількість статей витрат:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnExit);
+            this.groupBox2.Controls.Add(this.btnCreate);
+            this.groupBox2.Controls.Add(this.chart1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 116);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(776, 322);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Діаграма витрат";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(658, 272);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(112, 44);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Вихід";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(658, 19);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(112, 44);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "Згенерувати діаграму";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Area3DStyle.Enable3D = true;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(6, 19);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.IsValueShownAsLabel = true;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(627, 297);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title1.DockedToChartArea = "ChartArea1";
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            title1.IsDockedInsideChartArea = false;
+            title1.Name = "Title1";
+            title1.Text = "Витрати компанії";
+            this.chart1.Titles.Add(title1);
+            // 
+            // frmCosts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmCosts";
+            this.Text = "Витрати";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCosts)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numCostsCount)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnCreateMas;
+        private System.Windows.Forms.NumericUpDown numCostsCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvCosts;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ task3/frmCosts.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace task3
+{
+    public partial class frmCosts : Form
+    {
+        readonly string[] strCosts =
+        {
+            "Комунальні платежі",
+            "Зарплата",
+            "Оренда",
+            "Потадки",
+            "Витратні матеріали",
+            "Транспорт",
+            "Зв'язок",
+            "Цифрові ліцензії",
+            "Технічна підтримка",
+            "Канцелярія",
+        };
+
+        public frmCosts()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateMas_Click(object sender, EventArgs e)
+        {
+            int CostsCount = (int)numCostsCount.Value;
+
+            dgvCosts.Rows.Clear();
+            dgvCosts.Columns.Clear();
+
+            dgvCosts.ColumnCount = CostsCount;
+            foreach (DataGridViewColumn col in dgvCosts.Columns)
+            {
+                col.HeaderText = (col.Index + 1).ToString();
+                col.Width = 100;
+            }
+
+            dgvCosts.Rows.Add();
+            dgvCosts.Rows.Add();
+
+            int[] rnd = RandomCosts(CostsCount);
+
+            for(int i = 0; i < CostsCount; i++)
+            {
+                dgvCosts.Rows[0].Cells[i].Value = strCosts[rnd[i]];
+                dgvCosts.Rows[1].Cells[i].Value = "0";
+            }
+        }
+
+        private int[] RandomCosts(int count)
+        {
+            int[] result = Enumerable.Repeat(-1, count).ToArray();
+
+            Random rnd = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                int j;
+                do j = rnd.Next(0, 10);
+                while (result.Contains(j));
+                result[i] = j;
+            }
+
+            return result;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            string[] X = new string[dgvCosts.ColumnCount];
+            int[] Y = new int[dgvCosts.ColumnCount];
+            int Sum = 0;
+
+            for (int i = 0; i < dgvCosts.ColumnCount; i++)
+            {
+                X[i] = dgvCosts.Rows[0].Cells[i].Value.ToString();
+                if (!Int32.TryParse(dgvCosts.Rows[1].Cells[i].Value.ToString(), out Y[i]))
+                {
+                    MessageBox.Show("У стовпці \"" + X[i] + "\" (" + (i+1) + ") виявлене неправильне значення!", "Помилка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Y[i] = 0;
+                }
+                Sum += Y[i];
+            }
+
+            if (Sum != 100)
+            {
+                MessageBox.Show("Некоретно введені дані!\nСума по всім статтям витрат становить \"" + Sum + "\", що не дорівнює 100 (%)");
+                return;
+            }
+
+            chart1.Series[0].Points.DataBindXY(X, Y);
+        }
+    }
+}
